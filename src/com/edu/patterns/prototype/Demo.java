f53f8fd916424366d1aa5a6a36beac9b03eb5d6b
ignacio <ignacio.a.r@hotmail.com>
2018-07-15 15:28:44 -0300

    Agregado patron prototype

---
+++ src/com/edu/patterns/prototype/Demo.java
@@ -0,0 +1,19 @@
+package com.edu.patterns.prototype;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		Person person1 = new Person("Fred");
+		System.out.println("person 1:" + person1);
+		Person person2 = (Person) person1.doClone();
+		System.out.println("person 2:" + person2);
+
+		Dog dog1 = new Dog("Wooof!");
+		System.out.println("dog 1:" + dog1);
+		Dog dog2 = (Dog) dog1.doClone();
+		System.out.println("dog 2:" + dog2);
+
+	}
+
+}
---
+++ src/com/edu/patterns/prototype/Prototype.java
@@ -0,0 +1,7 @@
+package com.edu.patterns.prototype;
+
+public interface Prototype {
+
+	public Prototype doClone();
+
+}
